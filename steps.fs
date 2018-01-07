4b28ec1ba4896c54e23c72f3c9b61a76db3b7e2d
unknown <m.shanti@windowslive.com>
2018-01-07 03:34:35 +0200

    step5

--- steps.fs
+++ steps.fs
@@ -1,4 +1,6 @@
 open System
+open System.Collections.Generic
+exception InnerError of List<int>
 
 let addArray (arr) = Array.sum arr
 
@@ -6,6 +8,18 @@ let strContainsOnlyNumber (s:string) = System.Int32.TryParse s |> fst
 
 let strip (stripChars:string) (text:string) =
     text.Split(stripChars.ToCharArray(), StringSplitOptions.RemoveEmptyEntries) |> String.Concat
+    
+
+let checkForNegatives (arr)= 
+  let temp = new List<int>()
+  let positive = new List<int>()
+  let mutable x = 0
+  for ar in arr do
+    match ar < 0 with
+    | true -> temp.Add(ar)
+    | false -> positive.Add(ar)
+  temp
+   
     
 let spliter (strs : string, splitType) = 
   let mutable nums = Array.zeroCreate 1
@@ -23,19 +37,28 @@ let spliter (strs : string, splitType) =
   nums
 
 let findDelimiter (inp: string)= 
-  let mutable delimiter = ""
-  if inp.[0] = '/' then
-    delimiter <- string(inp.[2])
+  let mutable delimiter = ","
+  if inp.Length > 0 then
+    if inp.[0] = '/' then
+      delimiter <- string(inp.[2])
+    else delimiter <- ","
   else delimiter <- ","
   delimiter
-    
+  
 let input = Console.ReadLine()
 let delimiter = (findDelimiter (input))
 let input2 = input.Replace("\\n",delimiter)
 let nums = spliter (input2, delimiter.[0])
-
-let answer = addArray (nums)
-printfn "%d" answer
+let temp = checkForNegatives (nums)
+if not(temp.Count > 0) then
+  let answer = addArray (nums)
+  printfn "%d" answer
+else
+  try
+    raise (InnerError(temp))
+  with
+    | InnerError(temp) -> printfn "negatives not allowed %A" temp
+  
 
 
 
